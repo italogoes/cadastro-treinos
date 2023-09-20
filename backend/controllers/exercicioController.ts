1d6e57eac08cfa695dd0ca34de15fb7ac1ffe704
Ítalo Góes <italogoes85@gmail.com>
2023-09-19 22:32:49 -0300

    criação da rota get

--- backend/controllers/exercicioController.ts
+++ backend/controllers/exercicioController.ts
@@ -1,4 +1,5 @@
 const exercicioModel = require('../models/exercicioModel')
+const multer = require('multer')
 
 interface exercicio {
     nome: String,
@@ -6,14 +7,40 @@ interface exercicio {
     diaSemana: any
 }
 
-exports.createExercicio = async (req: any, res: any) => {
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function (req: any, file: any, cb: any) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+const upload = multer({ storage: storage });
+
+exports.createExercicio = upload.single('image'), async (req: any, res: any) => {
     try {
         const { nome, descricao, diaSemana }: exercicio = req.body;
-        const imagePath = req.file ? req.file.path : ''; // Caminho da imagem
-        const exercicio = new exercicioModel({ nome, descricao, diaSemana, imagePath })
-        await exercicio.save()
-        res.status(201).json(exercicio)
+        console.log(nome)
+
+        if(nome != '' || descricao != ''){
+            const imagePath = req.file ? req.file.path : ''; // Caminho da imagem
+            const exercicio = new exercicioModel({ nome, descricao, diaSemana, imagePath })
+            await exercicio.save()
+            res.status(201).json(exercicio)
+        } else {
+            res.status(400).json({ message: 'Erro ao criar tarefa.' })
+            return
+        }
+        
     } catch (error) {
         res.status(400).json({ message: 'Erro ao criar tarefa.'})
+        return
+    }
+}
+
+exports.searchExercicio = async (req: any, res: any) => {
+    try {
+        const dados = await exercicioModel.find({})
+        res.json(dados)
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao buscar os dados no banco...', error })
     }
 }
--- backend/index.ts
+++ backend/index.ts
@@ -1,22 +1,30 @@
 import express from 'express';
 const mongoose = require('mongoose');
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
+// Controllers
 const exercioController = require('./controllers/exercicioController')
 
 const app = express()
 const port = 4000
 
+app.use(bodyParser.json());
+app.use(cors())
+
+// Routes
 app.get('/', (req: any, res: any) => {
     res.send('ola')
 })
 
+app.get('/exercicios', exercioController.searchExercicio)
 app.post('/exercicios', exercioController.createExercicio)
 
 app.listen(port, () => {
     console.log(`Servidor ligado na porta: ${port}!`)
 })
 
-const uri = 'mongodb+srv://projeto-cadastro-academia:projeto-cadastro-academia@projeto-cadastro-exerci.hxblypq.mongodb.net/?retryWrites=true&w=majority';
+const uri: string = 'mongodb+srv://projeto-cadastro-academia:projeto-cadastro-academia@projeto-cadastro-exerci.hxblypq.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
